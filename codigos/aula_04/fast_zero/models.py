72ddb340a049cf85f5aae010d608d5fce1506b06
jocelio <joceliojairovf@gmail.com>
2024-07-06 11:28:46 -0300

    continuação aula 4

--- codigos/aula_04/fast_zero/models.py
+++ codigos/aula_04/fast_zero/models.py
@@ -1,15 +1,15 @@
 from datetime import datetime
-from sqlalchemy.orm import Mapped, registry
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, registry, mapped_column
 
 table_registry = registry()
-
 
 @table_registry.mapped_as_dataclass
 class User:
     __tablename__ = 'users'
 
-    id: Mapped[int]
-    username: Mapped[str]
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
-    email: Mapped[str]
-    created_at: Mapped[datetime]
+    email: Mapped[str] = mapped_column(unique=True)
+    created_at: Mapped[datetime] = mapped_column(init=False, server_default=func.now())
